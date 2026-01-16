6ae0d9788fed2cd8267ca80b818576fe7ec1a84d
Lander Brandt <landerbrandt@gmail.com>
2026-01-16 09:38:48 -0800

    fix: do not log URLs in release mode

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,28 @@ enum Commands {
     Run,
 }
 
+#[derive(PartialEq, Eq)]
+struct ClipboardText(String);
+
+impl std::fmt::Display for ClipboardText {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if cfg!(debug_assertions) { f.write_str(&self.0) } else { f.write_str("[redacted]") }
+    }
+}
+
+impl std::fmt::Debug for ClipboardText {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut output = f.debug_tuple("ClipboardText");
+        if cfg!(debug_assertions) {
+            output.field(&self.0);
+        } else {
+            output.field(&"[redacted]");
+        }
+
+        output.finish()
+    }
+}
+
 fn main() -> Result<(), Report> {
     let args = Args::parse();
     let command = args.command.unwrap_or_default();
@@ -92,10 +114,10 @@ fn main() -> Result<(), Report> {
         .context("failed to build HTTP client")?;
 
     let mut clipboard = Clipboard::new().unwrap();
-    let mut last_content = clipboard.get_text().ok();
+    let mut last_content = clipboard.get_text().ok().map(ClipboardText);
 
     loop {
-        let mut current = clipboard.get_text().ok();
+        let mut current = clipboard.get_text().ok().map(ClipboardText);
         if current != last_content {
             trace!("New clipboard text detected. Current: {current:?}, last: {last_content:?}");
             if let Some(ref mut new_clipboard_text) = current {
@@ -107,7 +129,7 @@ fn main() -> Result<(), Report> {
                     Ok(Some(new_url)) => {
                         // URL detected and updated
                         debug!("Cleaned URL: {new_url}");
-                        if let Err(e) = clipboard.set_text(&new_url) {
+                        if let Err(e) = clipboard.set_text(&new_url.0) {
                             error!("could not set clipboard text: {e:?}");
                         } else {
                             *new_clipboard_text = new_url;
@@ -128,7 +150,11 @@ fn main() -> Result<(), Report> {
 }
 
 /// Attempts to parse a URL and clean it
-fn clean_clipboard_text(text: &str, http_client: &mut Client, matchers: &[Matcher]) -> Result<Option<String>, Report> {
+fn clean_clipboard_text(
+    text: &ClipboardText,
+    http_client: &mut Client,
+    matchers: &[Matcher],
+) -> Result<Option<ClipboardText>, Report> {
     let span = debug_span!("clean_clipboard_text");
     let _enter = span.enter();
 
@@ -138,7 +164,7 @@ fn clean_clipboard_text(text: &str, http_client: &mut Client, matchers: &[Matche
         debug!("cleaning text")
     }
 
-    let mut text = Cow::Borrowed(text);
+    let mut text = Cow::Borrowed(&text.0);
     let mut final_result = Ok(None);
     let mut redirect_depth = 0;
     'url_loop: loop {
@@ -187,8 +213,6 @@ fn clean_clipboard_text(text: &str, http_client: &mut Client, matchers: &[Matche
                     let response =
                         http_client.head(parsed_url.clone()).send().context("failed to get redirect target")?;
 
-                    debug!("got response: {:?}", response);
-
                     if response.status().is_redirection() {
                         if let Some(location_header) = response.headers().get(LOCATION) {
                             text = Cow::Owned(
@@ -220,7 +244,7 @@ fn clean_clipboard_text(text: &str, http_client: &mut Client, matchers: &[Matche
         break;
     }
 
-    final_result
+    final_result.map(|opt| opt.map(ClipboardText))
 }
 
 #[cfg(feature = "service")]
